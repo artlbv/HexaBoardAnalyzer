5d9610817adbbcdcc60955e466c249c1b78fcc5e
Artur Lobanov <arturlbv@gmail.com>
2017-04-03 23:33:33 +0200

    channel correlation

--- plotNoise.py
+++ plotNoise.py
@@ -128,12 +128,20 @@ def readTree(fname, chip = 0, sca = 0, nchans = 64, chan_select = "all"):
         # Pedestal subtraction
         for var,values in chan_data.items():
             chan_ped = values.mean()
-            #print chan, chan_ped
-
-            # subtract pedestal from values
-            all_chan_data[chan][var] = np.subtract(values,chan_ped)
+            chan_ped_std = values.std()
+            if chan_ped_std > 3.0:
+                print(80*"!")
+                print chan, chan_ped, chan_ped_std
+                # put channel to zero
+                all_chan_data[chan][var] = np.subtract(values,10000)
+            else:
+                # subtract pedestal from values
+                all_chan_data[chan][var] = np.subtract(values,chan_ped)
+                #if chan < 2:
+                #    print all_chan_data[chan][var]
 
     print("Finished event loop")
+
     tfile.Close()
 
     return all_chan_data
@@ -149,6 +157,7 @@ def calcNoise(all_chan_data):
         noise_data[var + "_CN"] = []
         noise_data[var + "_DS"] = []
         noise_data[var + "_AS"] = []
+        noise_data[var + "_large_sumDS"] = []
 
     for var in variabs:
         # Loop over events (based on 0 channel)
@@ -156,10 +165,15 @@ def calcNoise(all_chan_data):
 
             sumAS = 0
             sumDS = 0
+            n_valid_chans = 0
 
             for i,chan in enumerate(chans):
 
                 val = all_chan_data[chan][var][event]
+
+                if val > -999: n_valid_chans += 1
+                else: continue
+
                 # direct sum
                 sumDS += val
                 # alternate sum
@@ -167,8 +181,9 @@ def calcNoise(all_chan_data):
                 else: sumAS -= val
 
             # calc noise
-            inc_noise = sumAS / math.sqrt(len(chans))
-            coh_noise = math.sqrt(abs(sumDS * sumDS - sumAS * sumAS)) / len(chans)
+            #if n_valid_chans < 63: print(n_valid_chans)
+            inc_noise = sumAS / math.sqrt(n_valid_chans)
+            coh_noise = math.sqrt(abs(sumDS * sumDS - sumAS * sumAS)) / n_valid_chans
 
             if abs(sumAS) > 500:
                 print("Suspiciously large AS: %i in event %i" %(sumAS,event))
@@ -176,6 +191,10 @@ def calcNoise(all_chan_data):
             if abs(sumDS) > 500:
                 print("Suspiciously large DS: %i in event %i" %(sumDS,event))
                 continue
+
+            if sumDS > 150:
+                #print("SumDS: %f, event %i" % (sumDS, event))
+                noise_data[var + "_large_sumDS"].append(event)
 
             noise_data[var + "_AS" ].append(sumAS)
             noise_data[var + "_DS" ].append(sumDS)
@@ -193,7 +212,8 @@ def plotNoise(noise_data, cname):
     hists = []
     #for key,values in noise_data.items():
 
-    h_order = ["_DS","_AS"]#,"_IN","_CN"]#,"_CNF"]
+    #h_order = ["_DS","_AS"]#,"_IN","_CN"]#,"_CNF"]
+    h_order = ["_DS","_AS"]#,"_large_sumDS"]
     variabs = ["lg","hg"]
 
     #for key in sorted(data):
@@ -203,30 +223,72 @@ def plotNoise(noise_data, cname):
             key = var + htype
 
             values = noise_data[key]
+            if len(values) == 0: values = np.array([0])
             print key, values.mean(),values.std()
 
             xmin = math.floor(values.min())-0.5
             xmax = math.ceil(values.max())+0.5
-            #nbins = int((xmax-xmin))
-            nbins = min(100,len(values)/50)
+            nbins = int((xmax-xmin))/2
+            #nbins = min(100,len(values)/50)
 
             hist = rt.TH1F("h_" + key, key , nbins, xmin, xmax)
             for val in values: hist.Fill(val)
             #hist.Draw()
             hists.append(hist)
 
-    canv = rt.TCanvas("canv_noise","canv",1400,800)
+    canv = rt.TCanvas("canv_noise","canv",1000,800)
     #canv.Divide(4,len(hists)/4,0.01,0.01)
     canv.Divide(len(h_order),len(hists)/len(h_order),0.01,0.01)
     for i, hist in enumerate(hists):
         canv.cd(i+1)
         hist.Draw()
 
-    canv.Draw()
+    #canv.Draw()
     canv.Update()
 
     #q = raw_input("Continue?")
     canv.SaveAs(cname+".pdf")
+
+def calcCorr(all_chan_data, cname = "corr_plot.pdf"):
+    rt.gStyle.SetOptStat(0)
+    rt.gStyle.SetPadRightMargin(0.15)
+
+    chans = all_chan_data.keys()#[:3]
+    variabs = all_chan_data[chans[0]].keys()
+
+    hists = {}
+    for var in variabs:
+        hist = rt.TH2F("h_corr_"+var,"correlation for "+var,64,0,64,64,0,64)
+        hists["h_corr_"+var] = hist
+
+    for chan1 in chans:
+        for chan2 in chans:
+
+            for var in variabs:
+                if chan1 == chan2:
+                    corr = 0
+                else:
+                    corr_matr = np.corrcoef(all_chan_data[chan1][var],all_chan_data[chan2][var])
+                    corr = abs(corr_matr[0][1])
+                    #print corr[0][1]
+                hists["h_corr_"+var].SetBinContent(chan1+1,chan2+1,corr)
+
+    canv = rt.TCanvas("canv_noise","canv",1000,500)
+    canv.Divide(len(hists),1,0.01,0.01)
+
+    for i,hname in enumerate(hists):
+        hist = hists[hname]
+        canv.cd(i+1)
+        hist.Draw("colz")
+
+    canv.Update()
+    canv.Draw()
+    rt.gStyle.SetOptStat(1)
+
+    #q = raw_input("continue?")
+
+    canv.SaveAs(cname+".pdf")
+
 
 if __name__ == "__main__":
 
@@ -250,13 +312,18 @@ if __name__ == "__main__":
     #chip = 0
     sca = 4
     nchans = 64
-    chan_select = "even"
+    chan_select = "all"
 
     for chip in range(4):
         print(80*"#")
         print("Analyzing: chip %i, sca %i" %(chip,sca))
 
         all_data = readTree(fname, chip, sca, nchans, chan_select)
+
+        cname = run_dir + "corr_chip_%i_sca_%i_chans_%s" %(chip,sca,chan_select)
+        calcCorr(all_data, cname)
+
         cname = run_dir + "noise_chip_%i_sca_%i_chans_%s" %(chip,sca,chan_select)
         noise_data = calcNoise(all_data)
         plotNoise(noise_data, cname)
+
