4c3beb13d88c0184ad8e2a6132b316d3c1bce600
Artur Lobanov <arturlbv@gmail.com>
2017-05-13 16:36:49 +0200

    extra plot scripts

---
+++ plot_HVscan.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python2
+import os
+import ROOT# as rt
+ROOT.gStyle.SetOptStat(0)
+
+ofile = ROOT.TFile("hv_scan.root","recreate")
+file_map = []
+#file_map.append(("/Users/artur/Work/LLR/HGCAL/Testbeam/data/may17/PED_RUN_0712_100517_1417.root","unplugged"))
+
+#file_map.append(("/Users/artur/Work/LLR/HGCAL/Testbeam/data/may17/PED_RUN_0713_100517_1423.root","0/ plugged"))
+'''
+file_map.append(("/Users/artur/Work/LLR/HGCAL/Testbeam/data/may17/PED_RUN_0715_100517_1432.root","5V",30))
+file_map.append(("/Users/artur/Work/LLR/HGCAL/Testbeam/data/may17/PED_RUN_0717_100517_1440.root","20V",34))
+file_map.append(("/Users/artur/Work/LLR/HGCAL/Testbeam/data/may17/PED_RUN_0718_100517_1444.root","50V",38))
+file_map.append(("/Users/artur/Work/LLR/HGCAL/Testbeam/data/may17/PED_RUN_0719_100517_1448.root","100V",42))
+file_map.append(("/Users/artur/Work/LLR/HGCAL/Testbeam/data/may17/PED_RUN_0720_100517_1452.root","150V",45))
+file_map.append(("/Users/artur/Work/LLR/HGCAL/Testbeam/data/may17/PED_RUN_0721_100517_1456.root","200V",47))
+'''
+file_map.append(("/Users/artur/Work/LLR/HGCAL/Testbeam/data/may17/PED_RUN_0722_100517_1500.root","250V",50))
+#file_map.append(("/Users/artur/Work/LLR/HGCAL/Testbeam/data/may17/PED_RUN_0711_100517_1404.root","250V orig",2))
+#file_map.append(("/Users/artur/Work/LLR/HGCAL/Testbeam/data/may17/PED_RUN_0126_120517_1529.root","New",1))
+file_map.append(("/Users/artur/Work/LLR/HGCAL/Testbeam/data/may17/PED_RUN_0127_120517_1544.root","New",1))
+#file_map.append(("/Users/artur/Work/LLR/HGCAL/Testbeam/data/may17/PED_RUN_0047_100517_2054.root","cover"))
+#file_map.append(("/Users/artur/Work/LLR/HGCAL/Testbeam/data/may17/PED_RUN_0046_100517_2022.root","cover + GND"))
+
+canv = ROOT.TCanvas("canv","canv",1000,600)
+ROOT.SetOwnership(canv,0)
+
+#sk2cms.Draw("hg[0][][10]","roll[Iteration$] == 0 && event < 900","colz"); c.Draw()
+pOpt = "hist"
+
+hists = []
+for fname,label,col in file_map:
+    hname = "h" + label
+
+    tfile = ROOT.TFile(fname)
+    tree = tfile.Get("sk2cms")
+    if not tree: continue
+
+    hist = ROOT.TH1F(hname,label,100,0,500)
+    #tree.Draw("hg[0][][10] >>" + hname + "(100,0,1000)","roll[Iteration$] == 0",pOpt);
+    #tree.Draw("hg[0][][9] >>" + hname,"roll[Iteration$] == 0",pOpt);
+    tree.Draw("hg[0][][10] >>" + hname,"roll[Iteration$] == 0",pOpt);
+    #tree.Draw("lg[0][][9] >>" + hname,"roll[Iteration$] == 0",pOpt);
+    #tree.Draw("hg[0][][9] >>" + hname,"roll[Iteration$] == 0",pOpt);
+    hist.SetDirectory(ofile)
+
+    hist.SetLineColor(col)
+    hist.SetLineWidth(3)
+    hist.GetXaxis().SetTitle("ADC")
+    '''
+    hist = ROOT.gDirectory.Get(hname)#.Clone(hname)
+    if not hist:
+        print "fail", hname
+        continue
+    else:
+        hist = hist.Clone(hname)
+        #ROOT.SetOwnership(hist,0)
+        hist.SetDirectory(ofile)
+    '''
+    print hist
+    hists.append(hist)
+    #print fname,label
+
+    #break
+    #if "same" not in pOpt:
+    pOpt = pOpt + " same"
+
+print hists
+canv.Update()
+canv.SetLogy()
+canv.Draw()
+canv.Update()
+q = raw_input()
+
+ofile.Close()
---
+++ plot_mip.py
@@ -0,0 +1,101 @@
+#!/usr/bin/env python2
+import ROOT
+import numpy as np
+
+def getHist(tree,maxTS):
+
+    hname = "hMaxTS"+str(maxTS)
+    hist = ROOT.TH1F(hname,hname,250,-100,150)
+    #hist = ROOT.TH1F(hname,hname,3000,0,3000)
+
+    chan_i = 34
+    chip_i = 0
+
+    for entry in tree:
+
+    #if tree.event > 10: break
+
+        did_toa_fire = False
+        for chip in [chip_i]:#range(4):
+            for chan in range(0,64,2):
+                if tree.toa_rise[chip * 64 + chan] > 0 or tree.tot_fast[chip * 64 + chan] > 0:
+                    did_toa_fire = True;
+                    break
+            if did_toa_fire: break
+        if did_toa_fire: continue
+
+        maxSCA = -1
+        #roll = tree.roll
+        for ts in range(13):
+            if tree.timesamp[ts] == maxTS:
+                maxSCA = ts
+                break
+
+        if maxSCA == -1:
+            print "ha"
+            exit()
+        #elif maxSCA != 0: continue
+
+        charges = []
+        for chip in [chip_i]:#range(4):
+            for chan in range(0,64,2):
+                if abs(chan - chan_i) < 4 and chip == chip_i: continue
+                charge = tree.hg[chip * 13 * 64 + maxSCA * 64 + chan]
+                #if charge < 2000 and charge > 0:
+                charges.append(charge)
+
+        #if len(charges) < 32: continue
+        mean = int(np.median(charges))
+        #mean = np.mean(charges)
+        if mean < 4: continue
+        signal = tree.hg[chip_i * 13 * 64 + maxSCA * 64 + chan_i]
+        if signal < 4: continue
+        #if signal == mean : continue
+
+        #if abs(signal-mean) < 4: print signal, mean
+        #if abs(signal-mean) < 2: continue
+        shift = 10
+        hist.Fill(int(signal - mean - shift))
+        '''
+        if maxTS == 0:
+            hist.Fill(np.median(charges))
+        elif maxTS == 3:
+            hist.Fill(np.mean(charges))
+        #print mean
+        '''
+
+    return hist
+
+if __name__ == "__main__":
+
+    fname = "merge.root"
+
+    tfile = ROOT.TFile(fname)
+
+    tree = tfile.Get("sk2cms")
+
+    #hist = ROOT.TH1F("hist","hist",200,-50,150)
+    #hist = ROOT.TH1F("hist","hist",3000,-250.5,2500.5)
+
+
+    hist_ts3 = getHist(tree,3);
+    hist_ts0 = getHist(tree,0)
+
+    h_diff = hist_ts3.Clone("diff")
+
+    hist_ts3.SetLineColor(ROOT.kRed)
+    h_diff.SetLineColor(ROOT.kBlack)
+
+    ts0_max = hist_ts0.GetMaximum()
+    ts3_max = hist_ts3.GetMaximum()
+
+    hist_ts0.Scale(ts3_max/ts0_max)
+    #hist_ts3.Add(hist_ts0,-ts3_max/ts0_max)
+    h_diff.Add(hist_ts0,-1)#-ts3_max/ts0_max)
+
+    h_diff.Fit("landau","R","",20,120)
+
+    hist_ts0.Draw("hist")
+    hist_ts3.Draw("hist same")
+    h_diff.Draw("hist same")
+    q = raw_input("")
